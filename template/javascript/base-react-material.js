085b877ecb781b3b1b037ad3538e4b91d697835c
DuGlaser <bjan1290569@gn.iwasaki.ac.jp>
2019-12-01 14:33:17 +0900

    :recycle:fix react-material template
    ~

---
+++ template/javascript/base-react-material.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+
+const useStyle = makeStyles({});
+
+const Picker = () => {
+  const classes = useStyle();
+  return;
+};
+export default Picker;
