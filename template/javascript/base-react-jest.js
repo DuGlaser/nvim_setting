a6d2dcff2baaf3a3ebc6bc2c5e2f114980f3dca3
DuGlaser <duglaser0618@gmail.com>
2020-07-31 12:48:22 +0900

    :heavy_minus_sign: Del the sonic-template plugin

--- template/javascript/base-react-jest.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { mount } from "enzyme";
-
-test("renders learn react link", () => {
-  // shallow is not working
-  // https://github.com/emotion-js/emotion/issues/1372
-  const wrapper = mount();
-  expect(wrapper.find().length).toBe(1);
-});
