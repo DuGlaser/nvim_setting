23ac79adfc2af29b469a74acdef7b8889d2d516e
DuGlaser <duglaser0618@gmail.com>
2020-12-26 00:19:32 +0900

    :heavy_plus_sign: Add some plugins

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -4,16 +4,25 @@ return require('packer').startup(function()
   use 'gruvbox-community/gruvbox'
   use 'machakann/vim-sandwich'
   use 'christoomey/vim-tmux-navigator'
+  use {'neoclide/coc.nvim', run = 'yarn install --frozen-lockfile'}
   use {
     'akinsho/nvim-bufferline.lua',
     requires = {
       {'kyazdani42/nvim-web-devicons'},
     }
   }
+  use 'SirVer/ultisnips'
   use {
     'mattn/emmet-vim',
     cmd = 'EmmetInstall',
     opt = true
+  }
+  use {
+    'lewis6991/gitsigns.nvim',
+    requires = {
+      {'nvim-lua/popup.nvim'},
+      {'nvim-lua/plenary.nvim'},
+    }
   }
   use {
     'nvim-telescope/telescope.nvim',
@@ -26,6 +35,38 @@ return require('packer').startup(function()
     'nvim-treesitter/nvim-treesitter',
     run = function() vim.cmd [[TSUpdate]] end
   }
+  use 'romgrk/equal.operator'
+  use 'rhysd/clever-f.vim'
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-commentary'
+  use {
+    'kana/vim-operator-replace',
+    requires = {
+      {'kana/vim-operator-user'}
+    },
+    config = function() vim.cmd('map <Space>s <Plug>(operator-replace)') end
+  }
+  use {
+    't9md/vim-choosewin',
+    config = function() vim.cmd('nmap  -  <Plug>(choosewin)') end
+  }
+  use 'lambdalisue/gina.vim'
+  use {
+    'itchyny/vim-parenmatch',
+    config = function() vim.g.loaded_matchparen = 1 end
+  }
+  use {
+    'norcalli/nvim-colorizer.lua',
+    config = function() require'colorizer'.setup() end
+  }
+  use {
+    'thinca/vim-zenspace',
+    config = function() vim.cmd("let g:zenspace#default_mode = 'on'") end
+  }
+  use {
+    'machakann/vim-highlightedyank',
+    config = function() vim.g.highlightedyank_highlight_duration = 200 end
+  }
 
   -- fern
   use 'lambdalisue/fern.vim'
@@ -35,3 +76,5 @@ return require('packer').startup(function()
   use 'lambdalisue/nerdfont.vim'
   use 'lambdalisue/glyph-palette.vim'
 end)
+
+-- vim.cmd('autocmd BufWritePost plugins.lua PackerCompile')
