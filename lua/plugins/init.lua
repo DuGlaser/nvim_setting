6931a945054ef1f775b2901bd1b0bc8720f48fdd
DuGlaser <duglaser0618@gmail.com>
2020-12-26 00:20:14 +0900

    :sparkles: Add some plugins setting

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,3 +1,7 @@
 require 'plugins.plugins'
 require 'plugins.fern'
 require 'plugins.telescope'
+require 'plugins.treesitter'
+require 'plugins.bufferline'
+require 'plugins.ultisnips'
+require 'plugins.gitsigns'
