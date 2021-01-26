bcea57731ade867e1d68b374bc0b32086e727375
DuGlaser <duglaser0618@gmail.com>
2021-01-26 23:34:57 +0900

    :wrench: Add the clap keymap setting

--- lua/plugins/clap/keymap.lua
+++ lua/plugins/clap/keymap.lua
@@ -3,3 +3,4 @@ local map = vim.api.nvim_set_keymap
 map('n', '<Space>cf', '<cmd>Clap files<cr>', { noremap = true, silent = true })
 map('n', '<Space>cg', '<cmd>Clap grep<cr>', { noremap = true, silent = true })
 map('n', '<Space>cd', '<cmd>Clap grep ++query=<cword><cr>', { noremap = true, silent = true })
+map('n', '<Space>cb', '<cmd>Clap buffers', { noremap = true, silent = true })
