451df92721f07c4fd5328862927cf7b5fc0a702e
DuGlaser <duglaser0618@gmail.com>
2021-01-26 23:35:24 +0900

    :sparkles: Add the bufferline keymap setting

--- lua/plugins/bufferline/keymap.lua
+++ lua/plugins/bufferline/keymap.lua
@@ -1,6 +1,6 @@
 local remap = vim.api.nvim_set_keymap
 
 remap('n','<C-t><C-t>', '<cmd>BufferLinePick<CR>', { noremap = true, silent = true })
-remap('n','<C-t><C-l>', '<cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true })
-remap('n','<C-t><C-h>', '<cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true })
+remap('n','<C-t>l', '<cmd>BufferLineCycleNext<CR>', { noremap = true, silent = true })
+remap('n','<C-t>h', '<cmd>BufferLineCyclePrev<CR>', { noremap = true, silent = true })
 remap('n','<C-t><C-p>', '<cmd>bw<CR>', { noremap = true, silent = true })
